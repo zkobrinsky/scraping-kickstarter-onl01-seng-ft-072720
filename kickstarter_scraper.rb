692e54f127d9428af8f953dfabb11f196eb45d18
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-13 16:42:26 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,7 @@ def create_project_hash
   project = kickstarter.css("li.project.grid_4").first
   title = project.css("h2.bbcard_name strong a").text
   image = project.css("div.project-thumbnail a img").attribute("src").value
+  blurb = project.css("p.bbcard_blurb").text.strip
   binding.pry
 end
 

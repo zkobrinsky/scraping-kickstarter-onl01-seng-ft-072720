8c2197ed972569105125d9b9c7618f0fc0498b9d
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-13 16:41:00 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,8 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   project = kickstarter.css("li.project.grid_4").first
+  title = project.css("h2.bbcard_name strong a").text
+  image = project.css("div.project-thumbnail a img").attribute("src").value
   binding.pry
 end
 

a72a3996e557b865871799f5fe691100fa40c646
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-13 17:09:27 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,8 +10,13 @@ def create_project_hash
   source_hash = kickstarter.css("li.project.grid_4")
   source_hash.each do |project|
     title = project.css("h2.bbcard_name strong a").text
-      projects[title.to_sym] = {}
-      binding.pry
+    projects[title.to_sym] = {
+    :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+    :description => project.css("p.bbcard_blurb").text,
+    :location => project.css("ul.project-meta span.location-name").text,
+    :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+  }
+
   end
 
 
